9f9d42f564823756d374d54caac24e0933b0a5d3
Hamza Kenbouch <hamzakenbouch.co@gmail.com>
2023-03-17 17:05:54 -0700

    Print Alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,39 +1,16 @@
 #include <stdio.h>
 /**
- * main - Print all possible combinations of two digits.
- * Numbers must be separated by commas and a space.
- * 01 and 10 are considered as the same combination of the two digits.
- * Print only the smallest combination of two digits.
- * Numbers should be printed in ascending order, with two digits.
- * You can only use `putchar`.
- * You can only use `putchar` up to 5 times.
- * You are not allowed to use any variable of type `char`.
+ * main - Use `putchar` function to print the alphabet in lowercase.
  * Return: 0
  */
 int main(void)
 {
-	int a, b, c;
+	char alpha = 'a';
 
-	a = 0;
-
-	while (a < 100)
+	while (alpha <= 'z')
 	{
-		b = a % 10; /* singles digit */
-		c = a / 10; /* doubles digit */
-
-		if (c < b)
-		{
-			putchar(c + '0');
-			putchar(b + '0');
-
-			if (a < 89)
-			{
-				putchar(',');
-				putchar(' ');
-			}
-		}
-
-		a++;
+		putchar(alpha);
+		alpha++;
 	}
 	putchar('\n');
 
