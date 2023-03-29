6e2e95aef20c5575db48a4d5acb489e120eec84d
Hamza Kenbouch <hamzakenbouch.co@gmail.com>
2023-03-29 15:40:27 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,10 +9,20 @@
 
 char *_strcat(char *dest, char *src)
 {
-	char *i = dest;
+	char *dest;
 	char *src;
 
-	strcat((*dest), (*src));
+	while (*dest != '\0')
+	{
+		dest++;
+	}
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
 
 	return (dest);
 }
