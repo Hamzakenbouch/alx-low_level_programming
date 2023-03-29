a5eb13d0c313641d44af50849f83cad4016f17e6
Hamza Kenbouch <hamzakenbouch.co@gmail.com>
2023-03-29 15:57:46 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,11 +2,11 @@
 
 /**
  * _strcat - concatenates two strings
- * @dest : char
- * @src : char
- *
- * return : returns a pointer to the resulting string dest
+ * @dest: char
+ * @src: char
+ * Return: char
  */
+
 char *_strcat(char *dest, char *src)
 {
 	char *a = dest;
