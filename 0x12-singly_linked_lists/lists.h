4fa32dcf129ca9e49cb7346f8e338fe699df6b0a
Hamza Kenbouch <hamzakenbouch.co@gmail.com>
2023-04-28 13:22:39 -0700

    Singly linked lists

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -21,6 +21,5 @@ typedef struct list_s
 	list_t *add_node(list_t **head, const char *str);
 	list_t *add_node_end(list_t **head, const char *str);
 	void free_list(list_t *head);
-	void printbuffer(void);
 
 #endif
