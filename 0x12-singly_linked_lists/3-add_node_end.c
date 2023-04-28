6e0b742032fffd81047186e88cf097f1a7f06335
Hamza Kenbouch <hamzakenbouch.co@gmail.com>
2023-04-28 13:28:37 -0700

    Singly linked lists

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -11,34 +11,34 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *nw_node;
+	list_t *new_node;
 	list_t *tp = *head;
 	unsigned int j = 0;
 
 	while (str[j])
 		j++;
 
-	nw_node = malloc(sizeof(list_t));
-	if (nw_node == NULL)
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
 		return (NULL);
 
-	nw_node->str = strdup(str);
-	nw_node->len = j;
-	if (nw_node->str == NULL)
+	new_node->str = strdup(str);
+	new_node->len = j;
+	if (new_node->str == NULL)
 	{
-		free(nw_node);
+		free(new_node);
 		return (NULL);
 	}
 
-	nw_node->next = NULL;
+	new_node->next = NULL;
 
 	if (*head == NULL)
 	{
-		*head = nw_node;
-		return (nw_node);
+		*head = new_node;
+		return (new_node);
 	}
 	while (tp->next)
 		tp = tp->next;
-	tp->next = nw_node;
-	return (nw_node);
+	tp->next = new_node;
+	return (new_node);
 }
